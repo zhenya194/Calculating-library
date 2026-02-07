a2d5a11c42e249dff1f46d4beeaf3a2865916a73
zhenya194 <zhenya.pro1753@gmail.com>
2026-02-07 16:43:42 +0200

    Update funcs

--- calc.py
+++ calc.py
@@ -4,14 +4,14 @@
 def plus(*numbers):
     result:int = numbers[0]
     i:int = 1
-    for i in numbers:
+    for i in range(len(numbers)):
         result += numbers[i]
     return result
 
 def minus(*numbers):
     result:int = numbers[0]
     i:int = 1
-    for i in numbers:
+    for i in range(len(numbers)):
         result -= numbers[i]
     return result
 
@@ -25,7 +25,7 @@ def multiply(*numbers):
 def divide(*numbers): 
     result:int = numbers[0]
     i:int = 1
-    for i in numbers:
+    for i in range(len(numbers)):
         result //= numbers[i]
     return result
 
