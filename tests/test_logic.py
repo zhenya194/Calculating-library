e83984682c560344778a1f82b08ca4b1653a1a3d
zhenya194 <zhenya.pro1753@gmail.com>
2026-02-10 19:33:15 +0200

    Add some tests

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -16,7 +16,7 @@ def test_minus():
 def test_minus():
     assert minus(100, 200, 13) == -113
 def test_minus():
-    assert minus(-7, -3) == -10
+    assert minus(-7, -3) == -4
 
 def test_multiply():
     assert multiply(4, 20) == 80
@@ -29,12 +29,36 @@ def test_multiply():
 
 def test_divide():
     assert divide(25, 5) == 5
+def test_divide():
+    assert divide(3, 3) == 1
+def test_divide():
+    assert divide(100, 10, 5) == 2
+def test_divide():
+    assert divide(300, 30) == 10
 
 def test_dividef():
     assert dividef(27, 5) == 5.4
+def test_dividef():
+    assert dividef(25, 5) == 5
+def test_dividef():
+    assert dividef(100, 30) == 3.3
+def test_dividef():
+    assert dividef(39, 3) == 13
 
 def test_divreminder():
     assert divreminder(27, 5) == 2
+def test_divreminder():
+    assert divreminder(29, 2) == 1
+def test_divreminder():
+    assert divreminder(27, 3) == 0
+def test_divreminder():
+    assert divreminder(56, 6) == 2
 
 def test_degree():
     assert degree(2, 3) == 8
+def test_degree():
+    assert degree(5, 2) == 25
+def test_degree():
+    assert degree(3, 3) == 27
+def test_degree():
+    assert degree(10, 2) == 100
